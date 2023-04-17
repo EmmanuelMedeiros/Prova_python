803bcd07274118a870c473d37ad09dc304622908
EmmanuelMedeiros <emmanuel.t2003@hotmail.com>
2023-04-17 12:01:34 -0300

    first moves

---
+++ react_app/my_app/src/App.js
@@ -0,0 +1,13 @@
+import MainScreen from "./components/MainScreen";
+import LoginScreen from "./components/LoginScreen";
+import RegisterScreen from "./components/RegisterScreen"
+
+function App() {
+  return(
+    <div>
+      <RegisterScreen/>
+    </div>
+  )
+}
+
+export default App;
---
+++ react_app/my_app/src/components/MainScreen.jsx
@@ -0,0 +1,13 @@
+import "./styles/mainScreen.css"
+
+function MainScreen() {
+    return(
+        <div className="wrapper main_container">
+            <h1>Welcome to users_app</h1>
+            <h2>Register</h2>
+            <h2>Login</h2>
+        </div>
+    )
+}
+
+export default MainScreen
