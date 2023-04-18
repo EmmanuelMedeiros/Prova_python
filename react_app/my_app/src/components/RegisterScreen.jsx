172bf62b1760a75d5ca4dc3b600020b158d47d6f
EmmanuelMedeiros <emmanuel.t2003@hotmail.com>
2023-04-18 10:23:49 -0300

    everything is going to change

--- react_app/my_app/src/components/RegisterScreen.jsx
+++ react_app/my_app/src/components/RegisterScreen.jsx
@@ -3,24 +3,14 @@ import "./styles/registerScreen.css"
 function RegisterScreen() {
     return(
         <div className="wrapper register_container">
-            <h1>Register to your account</h1>
+            <h1>Register account</h1>
 
             <form>
                 <div className="sections">
                 <label><h2>Username:</h2></label>
                 <input type="text" name="" id="" />
                 </div>
-
-                <div className="sections">
-                <label><h2>Nickname: </h2></label>
-                <input type="text" name="" id="nick_input" />
-                </div>
-
-                <div className="sections">
-                <label><h2>Age:</h2></label>
-                <input type="text" name="" id="age_input" />
-                </div>
-
+                
                 <div className="sections">
                 <label><h2>Password:</h2></label>
                 <input type="password" name="" id="pwd_input" />
