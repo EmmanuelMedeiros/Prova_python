1d274306ce576dd362327ddab7bc60a67662046b
Emmanuel <emmanuelmedeiros05@gmail.com>
2023-04-19 15:59:05 -0300

    login screen finished

--- react_app/my_app/src/components/Home.jsx
+++ react_app/my_app/src/components/Home.jsx
@@ -21,10 +21,11 @@ function Home() {
     }
 
     return(
-        <div className="wrapper screen home_container">
+        <div className="wrapper teste screen home_container">
+            
             <ul className="logo_section">
                 <li><img src={logo}/></li>
-                <li><h1>Tome nota</h1></li>
+                <li><h1>Tome_nota</h1></li>
             </ul>
 
             <ul className="login_label">
@@ -34,26 +35,38 @@ function Home() {
 
             <form action="" className="login_section">
 
-                <div className="my_labels">
-                <label>Email</label>
+                <div className="my_labels email">
+                <label id="email_label">Email</label>
                 <input type="email" placeholder="example@hotmail.com" name="" id="email" />
                 </div>
 
-                <div className="my_labels">
+                <div className="my_labels pwd">
                 <label>Password</label>
                 <input type="password" placeholder="At least 4 characters" name="" id="pwd" />
                 </div>
             </form>
 
-
             <div className="styled_home">
                 <h1>Um app feito para lembretes e anotações</h1>
+
                 <ul className="style_block">
                     <li id="biggest_li"></li>
                     <li className="low_li"></li>
                     <li className="low_li"></li>
                     <li className="low_li"></li>
-                </ul>               
+                </ul>    
+
+                <ul className="another_block">
+                    <li id="biggest_li"></li>
+                    <li className="low_li"></li>
+                    <li className="low_li"></li>
+                </ul>          
+
+                <div className="end_phrase">
+                    <h2 id="only_full">Bem vindo de volta!</h2>
+                    <h3>Começe já a tomar nota dos seus afazeres</h3>
+                </div>
+
                 <p>@ 2023 ALL RIGHTS RESERVED</p>
             </div>
         </div>
