c96db8a2f8554749e3245ae2a7df40d2abea988f
EmmanuelMedeiros <emmanuel.t2003@hotmail.com>
2023-04-19 10:51:43 -0300

    starting media query

--- react_app/my_app/src/components/Home.jsx
+++ react_app/my_app/src/components/Home.jsx
@@ -1,9 +1,27 @@
 import "./styles/home.css"
 import  logo from "./commons/logo.png"
+import { useEffect, useState } from "react"
 
 function Home() {
+
+    const [state, set_state] = useState()
+
+    useEffect(() => {
+        console.log(window.screen.width)
+
+        console.log(state)
+    })
+
+    let for_mobile = true
+
+    if(state >= "768") {
+        for_mobile = false
+    } else {
+        for_mobile = true
+    }
+
     return(
-        <div className="wrapper home_container">
+        <div className="wrapper screen home_container">
             <ul className="logo_section">
                 <li><img src={logo}/></li>
                 <li><h1>Tome nota</h1></li>
@@ -26,6 +44,18 @@ function Home() {
                 <input type="password" placeholder="At least 4 characters" name="" id="pwd" />
                 </div>
             </form>
+
+
+            <div className="styled_home">
+                <h1>Um app feito para lembretes e anotações</h1>
+                <ul className="style_block">
+                    <li id="biggest_li"></li>
+                    <li className="low_li"></li>
+                    <li className="low_li"></li>
+                    <li className="low_li"></li>
+                </ul>               
+                <p>@ 2023 ALL RIGHTS RESERVED</p>
+            </div>
         </div>
     )
 }
