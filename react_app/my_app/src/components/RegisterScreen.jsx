107f0091f8313797803a814d4baf1abaa69ae89b
EmmanuelMedeiros <emmanuel.t2003@hotmail.com>
2023-04-24 09:57:14 -0300

    centralizing error box

---
+++ react_app/my_app/src/components/RegisterScreen.jsx
@@ -0,0 +1,9 @@
+function RegisterScreen() {
+    return(
+        <div className="wrapper screen register_container">
+            <h1>Register</h1>
+        </div>
+    )
+}
+
+export default RegisterScreen
