71b5ac4fbd4d4724adf2ae0ffb412b7cd4900160
EmmanuelMedeiros <emmanuel.t2003@hotmail.com>
2023-04-25 09:49:01 -0300

    Create note container created for mobile

--- react_app/my_app/src/components/CreateNote.jsx
+++ react_app/my_app/src/components/CreateNote.jsx
@@ -1,9 +1,24 @@
 import "./styles/createNote.css"
 
+import { useEffect, useState } from "react"
+
 function CreateNote() {
+
+    const [letters, set_letters] = useState("")
+
+    useEffect(() => {
+        console.log(letters)
+    })
+
     return(
         <div className="wrapper screen create_container">
-            <h1>criar nota</h1>
+            <h1>Nova nota</h1>
+
+            <form className="inserts">
+                <li id="insert_title"><input type="text" name="" placeholder="Título aqui" id="" /></li>
+                <p id={letters == "" ? "not_appear" : ""}>{-letters.length + 140}</p>
+                <li id="insert_content"><textarea type="text" name="" onChange={(event) => set_letters(event.target.value)} placeholder="Nova nota (até 140 caracteres)"  id="dd" /></li>
+            </form>
         </div>
     )
 }
